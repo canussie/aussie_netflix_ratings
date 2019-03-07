aa4a26585b38626fd869256b551c1362a21e8ace
canussie <canustralian@gmail.com>
2019-03-07 15:52:32 +1100

    change script to func

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aussie_netflix_ratings
 Project to link Aussie Netflix TV Series with Ratings such as IMDB
+
+## PreReqs
+
+
+
+## db.py
+
+Used to clean tables, create tables and import IMDB data into movies database.
+
+
--- scrape.py
+++ scrape.py
@@ -4,27 +4,28 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-url = "https://www.finder.com.au/netflix-tv-shows"
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-div = soup.find('div', class_="ts-table-container")
+def getshowlist():
+    url = "https://www.finder.com.au/netflix-tv-shows"
 
-tb = div.find('table', class_='luna-table')
-#print tb
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    div = soup.find('div', class_="ts-table-container")
 
-list = {}
+    tb = div.find('table', class_='luna-table')
+    #print tb
 
-for row in tb.find_all('tr'):
-    #print row
-    name = row.find('b')
-    year = row.find("td", {"data-title": "Year of release"})
-    temp = str(name)
-    temp2 = str(year)
-    #print type(name)
-    title = re.sub('<[^<]+?>', '', temp)
-    release = re.sub('<[^<]+?>', '', temp2)
-    #print title, release
-    list[title] = release
+    list = {}
 
-print list
+    for row in tb.find_all('tr'):
+        #print row
+        name = row.find('b')
+        year = row.find("td", {"data-title": "Year of release"})
+        temp = str(name)
+        temp2 = str(year)
+        #print type(name)
+        title = re.sub('<[^<]+?>', '', temp)
+        release = re.sub('<[^<]+?>', '', temp2)
+        #print title, release
+        list[title] = release
+    return list
