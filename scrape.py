9dba34c08f6194f0dfab962fa27cb51a1f19681f
canussie <canustralian@gmail.com>
2019-03-07 15:50:44 +1100

    added scraping script to get movie name and year from finder.com.au

---
+++ scrape.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url = "https://www.finder.com.au/netflix-tv-shows"
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+div = soup.find('div', class_="ts-table-container")
+
+tb = div.find('table', class_='luna-table')
+#print tb
+
+list = {}
+
+for row in tb.find_all('tr'):
+    #print row
+    name = row.find('b')
+    year = row.find("td", {"data-title": "Year of release"})
+    temp = str(name)
+    temp2 = str(year)
+    #print type(name)
+    title = re.sub('<[^<]+?>', '', temp)
+    release = re.sub('<[^<]+?>', '', temp2)
+    #print title, release
+    list[title] = release
+
+print list
